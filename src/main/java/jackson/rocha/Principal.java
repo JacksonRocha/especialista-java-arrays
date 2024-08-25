bd20972bee14cc4d5486afa37cfd1826924a62bd
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-25 15:29:38 -0300

    Projeto sobre arrays

---
+++ src/main/java/jackson/rocha/Principal.java
@@ -0,0 +1,14 @@
+package jackson.rocha;
+
+public class Principal {
+    public static void main(String[] args) {
+//        int[] notas = new int[5];
+
+//        int[] notas;
+//        notas = new int[]{8, 5, 4, 9, 10};
+
+//        int[] notas = new int[]{8, 5, 4, 9, 10};
+
+        int[] notas = {8, 5, 4, 9, 10};
+    }
+}
