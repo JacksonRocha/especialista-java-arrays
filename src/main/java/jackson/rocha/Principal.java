6446221e27867bf15b1d9eb4e542048441c032f4
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-25 15:58:22 -0300

    Percorrendo os arrays

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -2,19 +2,11 @@ package jackson.rocha;
 
 public class Principal {
     public static void main(String[] args) {
-        int[] notas = {8, 5, 4, 9, 10};
+        int[] notas = {8, 5, 6, 9, 10};
 
-        notas[0] = 10;
-        notas[1] = 9;
+        double media = Calculadora.calcularMedia(notas);
 
-        System.out.println(notas[0]);
-        System.out.println(notas[1]);
-        System.out.println(notas[3]);
+        System.out.println(media);
 
-        int totalNotas = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
-        System.out.println(totalNotas);
-
-        int posicao = 2;
-        System.out.println(notas[posicao - 1]);
     }
 }
