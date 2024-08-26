ffea0d1d9092d7f69095a4d53aa4495882fca2b1
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-26 15:53:17 -0300

    Adicionar a classe Turma

--- src/main/java/jackson/rocha/Turma.java
+++ src/main/java/jackson/rocha/Turma.java
@@ -1,10 +1,17 @@
 package jackson.rocha;
 
+import java.util.Arrays;
+
 public class Turma {
 
     String identificacao;
     String nomeProfessora;
-    Aluno[] alunos;
+    Aluno[] alunos = new Aluno[0];
+
+    void adicionarAluno(Aluno aluno) {
+        this.alunos = Arrays.copyOf(this.alunos, this.alunos.length + 1);
+        alunos[this.alunos.length - 1] = aluno;
+    }
 
     void imprimirListaDeAlunos() {
         for (Aluno aluno : alunos) {
