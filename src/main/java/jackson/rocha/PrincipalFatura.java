a3a5f2b103cbf5ca91d7d9a76275b700f080a0dd
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-28 11:42:57 -0300

    Trabalhando com VarArgs

--- src/main/java/jackson/rocha/PrincipalFatura.java
+++ src/main/java/jackson/rocha/PrincipalFatura.java
@@ -7,17 +7,8 @@ public class PrincipalFatura {
         fatura.valorTotal = 1293.55;
 
         ServicoDeCobranca servicoDeCobranca = new ServicoDeCobranca();
-
-//        String[] emailsCobranca = {"joão@gmail.com", "maria@gmail.com"};
-
-//        servicoDeCobranca.pagar(fatura, new String[]{"joão@gmail.com", "maria@gmail.com"});
-
-//        servicoDeCobranca.pagar(fatura, new String[0]);
-//        servicoDeCobranca.pagar(fatura, new String[]{});
-
-//        servicoDeCobranca.pagar(fatura, "jackson@gmail.com");
-
-        servicoDeCobranca.pagar(fatura);
+        servicoDeCobranca.pagar(fatura, "Jackson@gmail.com", "Jakeline@gmail.com");
+        servicoDeCobranca.pagar(fatura, "Helen@gmail.com");
 
     }
 
--- src/main/java/jackson/rocha/ServicoDeCobranca.java
+++ src/main/java/jackson/rocha/ServicoDeCobranca.java
@@ -1,14 +1,24 @@
 package jackson.rocha;
 
+import java.util.Objects;
+
 public class ServicoDeCobranca {
 
-    void pagar(Fatura fatura, String... emailsCobranca) {
+    void pagar(Fatura fatura, String emailCobranca, String... emailsAdicionais) {
+        Objects.requireNonNull(fatura, "Informe a fatura");
+        Objects.requireNonNull(emailCobranca, "Informe um email");
+        
         System.out.printf("Fatura %d, no valor total de R$%.2f, foi paga!%n",
                 fatura.numero, fatura.valorTotal);
 
+        enviarNotificacao(fatura, emailCobranca);
 
-        for (String email : emailsCobranca) {
-            System.out.printf("Fatura %d enviada para %s%n", fatura.numero, email);
+        for (String email : emailsAdicionais) {
+            enviarNotificacao(fatura, email);
         }
+    }
+
+    private void enviarNotificacao(Fatura fatura, String email) {
+        System.out.printf("Fatura %d enviada para %s%n", fatura.numero, email);
     }
 }
